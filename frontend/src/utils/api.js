33313b75f01e6421d29bdbee27a5a8445748aa3a
Sumit Prajapati <prajapatisumitop@gmail.com>
2026-06-11 16:23:34 +0530

    fix API baseURL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
- baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://codesync-backend-nine.vercel.app/api",
   timeout: 30000,
 });
 
