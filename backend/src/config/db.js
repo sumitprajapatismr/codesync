62976d8a9c360ab7d103d0af55c605e087a0f107
Sumit Prajapati <prajapatisumitop@gmail.com>
2026-06-12 09:48:43 +0530

    fix mongodb connection for production

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      process.env.MONGO_URI || "mongodb://127.0.0.1:27017/codesync"
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
